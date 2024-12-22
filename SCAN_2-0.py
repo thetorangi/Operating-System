8d7d3e8f5a017a70b4f638c81a5728c65f686005
Torangi Shrinath <torangisb@gmail.com>
2024-12-22 11:28:16 +0530

    updated

--- SCAN_2-0.py
+++ SCAN_2-0.py
@@ -7,7 +7,7 @@ def scan(seek, head, direction):
     right = [x for x in seek if x > head]
 
     if direction.lower()[0] == 'l':
-        sequence = left[::-1] + right  [:-1:-1]
+        sequence = left[::-1] + right 
     elif direction.lower()[0] == 'r':
         sequence = right + left[::-1]  
     else:
