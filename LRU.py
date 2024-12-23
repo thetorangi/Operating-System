69f1aea80ca485b49942f66d95a273e9e9c3cf0b
Torangi Shrinath <torangisb@gmail.com>
2024-12-23 11:31:40 +0530

    done

--- LRU.py
+++ LRU.py
@@ -1,29 +1,33 @@
-def lru(pages,size):
-    frames=[None]*size
-    hit=0
-    miss=0
-    ptr=0
-    for page in pages :
-        print(page,end=" -> ")
-        if page in frames:
-            hit+=1
-            ptr = ((frames.index(page)+1)%size)
-            print(frames,end=" ")
-            print("hit")
-        else:
-            miss+=1
-            if None in frames :
-                idx=frames.index(None)
-                frames[idx]=page
-                ptr=int(ptr+1)%size
-            else:
-                frames[ptr]=page
-            print(frames,end=" ")
-            print("miss")
-    print("Miss are : ",miss)
-    print("\nhit are : ",hit)
-    
-size=int(input("Enter number of frames : "))
-pages=list(map(int,input("Enter pages : ").split()))
+def opt(pages, size):
+    frames = []  
+    hit = 0      
+    miss = 0     
 
-lru(pages,size)
+    for i in range(len(pages)): 
+        if pages[i] in frames:
+            hit += 1
+        else:
+            miss += 1  
+            if len(frames) < size:
+                frames.append(pages[i])  
+            else:
+                idx = float('inf')
+                far =float('inf')
+                for j in range(len(frames)):
+                    try:
+                        nxt=pages[:i-1:-1].index(frames[j])+i+1
+                    except ValueError:
+                        nxt = float('inf')
+                        idx=j
+                        break
+                    if nxt < far:
+                        far=nxt
+                        idx=j
+                frames[idx]=pages[i]
+    print("Misses are:", miss)
+    print("Hits are:", hit)
+
+size = int (input("Enter no of frames  : "))
+pages = list(map(int,input("Enter pages : ").split()))
+
+opt(pages, size)
